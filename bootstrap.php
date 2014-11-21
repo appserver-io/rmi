5775dbb6aee1815526a27a5b93ea48fca0c0a7b8
Tim Wagner <t.wagner@techdivision.com>
2014-11-21 18:10:38 +0100

    Fixing docblock in bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -11,13 +11,14 @@
  *
  * PHP version 5
  *
- * @category  Library
- * @package   TechDivision_PersistenceContainerProtocol
- * @author    Tim Wagner <tw@techdivision.com>
- * @copyright 2014 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link      https://github.com/techdivision/TechDivision_PersistenceContainerProtocol
- * @link      http://www.appserver.io
+ * @category   Appserver
+ * @package    Psr
+ * @subpackage PersistenceContainerProtocol
+ * @author     Tim Wagner <tw@appserver.io>
+ * @copyright  2014 TechDivision GmbH <info@appserver.io>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/appserver-io-psr/persistencecontainerprotocol
+ * @link       http://www.appserver.io
  */
 
 $loader = require 'vendor/autoload.php';
